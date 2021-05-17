72f145d692bee9ad34be68e23d4d5119ba0c5b87
Vinay Vishwkarma <45309427+vinayvishwkarma@users.noreply.github.com>
2021-05-17 13:31:09 +0300

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,5 +1,5 @@
 public class HelloWorld{
 	public static void main(String[] args){
-	System.out.println("Hello User Welcome to  Jenkins Training");
+	System.out.println("=========**** Hello User Welcome to  Jenkins Training ****=========");
 }
 }
