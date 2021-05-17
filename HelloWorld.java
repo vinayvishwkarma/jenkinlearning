ed262847c65dba3430eb33fac3be3b10b1866c06
Vinay Vishwkarma <45309427+vinayvishwkarma@users.noreply.github.com>
2021-05-17 13:57:58 +0300

    Delete HelloWorld.java

--- HelloWorld.java
+++
@@ -1,6 +0,0 @@
-public class HelloWorld{
-	public static void main(String[] args){
-	System.out.println("=========**** Hello User Welcome to  Jenkins Training ****=========");
-			System.out.println("polling scm");
-}
-}
